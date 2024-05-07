c2d696f91c1af74ae54279c81c7a3b930f69b4c3
aitorjaro <aitorjaro11@hotmail.com>
2024-05-07 13:42:33 +0200

    Scroll automatico en categorias solo para moviles

--- scriptCategorias.js
+++ scriptCategorias.js
@@ -3,7 +3,7 @@ document.querySelectorAll('.botonCategorias').forEach(btn => {
     btn.addEventListener('click', function () {
       let categoria = this.getAttribute('data-categoria');
       let productos = document.querySelectorAll('.articulo');
-
+  
       productos.forEach(prod => {
         if (prod.getAttribute('data-categoria') === categoria || categoria === 'todos') {
           prod.style.display = 'block';
@@ -12,5 +12,27 @@ document.querySelectorAll('.botonCategorias').forEach(btn => {
           prod.style.display = 'none';
         }
       });
+  
+      // Calcula la posición actual en la página
+      let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      // Establece un valor máximo de desplazamiento
+      let maxScroll = 300;
+      // Calcula el desplazamiento proporcional a la posición en la página
+      let scrollValue = maxScroll - scrollPosition * 0.1;
+  
+      // Asegúrate de que el desplazamiento no sea negativo
+      if (scrollValue < 0) {
+        scrollValue = 0;
+      }
+  
+      // Desplazamiento suave hacia abajo basado en la posición actual
+      window.scrollTo({
+        top: scrollPosition + scrollValue,
+        behavior: 'smooth'
+      });
     });
   });
+  
+  
+  
+  
