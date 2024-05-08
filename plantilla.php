c13879a440aa003ebd5a412481b9a71af6b44dcc
aitorjaro <aitorjaro11@hotmail.com>
2024-05-08 18:26:20 +0200

    Favicon

--- plantilla.php
+++ plantilla.php
@@ -20,6 +20,7 @@ if (isset($_SESSION["carrito"])) {
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="imagenes/cerezas.png" />
     <?php startblock('titulo'); ?>
     <?php endblock() ?>
     <?php startblock('estilo'); ?>
