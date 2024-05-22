cc2bd21e6f54a5e77e1c6057b227245c11fda0ee
aitorjaro <aitorjaro11@hotmail.com>
2024-05-22 12:57:07 +0200

    Cabeceras de envio de mail desde tienda@elrincondelchatin.com

--- modelo.php
+++ modelo.php
@@ -10,14 +10,14 @@ if (!defined('CON_CONTROLADOR')) {
  * @return array $articulos
  */
 
- require_once __DIR__ . '/vendor/autoload.php';
- 
- $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
- $dotenv->load();
- 
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 function conexion()
 {
-    $servidor =  $_ENV['DB_HOST'];
+    $servidor = $_ENV['DB_HOST'];
     $bbdd = $_ENV['DB_DBNAME'];
     $usuario = $_ENV['DB_USER'];
     $contrasena = $_ENV['DB_PASS'];
@@ -107,7 +107,8 @@ function busqueda_articulos($termino)
     }
     return $articulos;
 }
-function cargar_cestas(){
+function cargar_cestas()
+{
     $articulos = array();
     $con = conexion();
     if ($con) {
@@ -179,7 +180,7 @@ function crearParametrosPago()
         } elseif ($url_completa == "http://elrincondelchatin.com/index.php/redireccion") {
             $urlOK = "http://elrincondelchatin.com/index.php/tpv_ok";
         }
-        
+
         $urlKO = "https://elrincondelchatin.com/tpv_ko.php";
         $id = time();
         $amount = $amount * 100;
@@ -217,13 +218,14 @@ function crearParametrosPago()
     }
 }
 
-function crearPedido(){
+function crearPedido()
+{
     session_start();
     if (isset($_SESSION["carrito"])) {
 
         $carrito_mio = $_SESSION["carrito"];
         $total_articulos = 0;
-    
+
         foreach ($carrito_mio as $total_articulos_articulo) {
             $total_articulos += $total_articulos_articulo["cantidad"];
         }
@@ -237,24 +239,24 @@ function crearPedido(){
             echo '</script>';
             die();
         } else {
-    
+
             include "redsysHMAC256_API_PHP_7.0.0/apiRedsys.php";
-    
+
             // Se crea Objeto
             $miObj = new RedsysAPI;
-    
-    
+
+
             if (!empty($_POST)) {//URL DE RESP. ONLINE
-    
+
                 $version = $_POST["Ds_SignatureVersion"];
                 $datos = $_POST["Ds_MerchantParameters"];
                 $signatureRecibida = $_POST["Ds_Signature"];
-    
-    
+
+
                 $decodec = $miObj->decodeMerchantParameters($datos);
                 $kc = 'sq7HjrUOBfKmC576ILgskD5srU870gJ7'; //Clave recuperada de CANALES
                 $firma = $miObj->createMerchantSignatureNotif($kc, $datos);
-    
+
                 echo PHP_VERSION . "<br/>";
                 echo $firma . "<br/>";
                 echo $signatureRecibida . "<br/>";
@@ -262,12 +264,12 @@ function crearPedido(){
                     echo "FIRMA OK";
                     $dsResponse = $miObj->getParameter('Ds_Response');
                     $numeroPedido = $miObj->getParameter('Ds_Order');
-    
+
                     // Comprobar el estado del pago
                     // Los códigos de respuesta menores a 100 indican un pago aceptado
                     if ((int) $dsResponse < 100) {
                         echo "El pago ha sido aceptado. Código de respuesta: $dsResponse";
-    
+
                     } else {
                         echo "El pago no ha sido aceptado. Código de respuesta: $dsResponse";
                     }
@@ -276,20 +278,20 @@ function crearPedido(){
                 }
             } else {
                 if (!empty($_GET)) {//URL DE RESP. ONLINE
-    
+
                     $version = $_GET["Ds_SignatureVersion"];
                     $datos = $_GET["Ds_MerchantParameters"];
                     $signatureRecibida = $_GET["Ds_Signature"];
-    
-    
+
+
                     $decodec = $miObj->decodeMerchantParameters($datos);
                     $kc = 'sq7HjrUOBfKmC576ILgskD5srU870gJ7'; //Clave recuperada de CANALES
                     $firma = $miObj->createMerchantSignatureNotif($kc, $datos);
-    
+
                     if ($firma === $signatureRecibida) {
                         $dsResponse = $miObj->getParameter('Ds_Response');
                         $numeroPedido = $miObj->getParameter('Ds_Order');
-    
+
                         // Comprobar el estado del pago
                         // Los códigos de respuesta menores a 100 indican un pago aceptado
                         if ((int) $dsResponse < 100) { //Pago aceptado
@@ -303,32 +305,32 @@ function crearPedido(){
                             $codigoPostal = $_SESSION["codigopostal"];
                             $total = $_SESSION["totalSinEnvio"];
                             $totalConEnvio = $_SESSION["totalConEnvio"];
-    
+
                             //Inserción en la BD
                             $conexion = conexion();
                             $stmt = $conexion->prepare("INSERT INTO pedidos (referencia, fecha, nombre, apellidos, telefono, email, direccion, localidad, codigoPostal, total, totalConEnvio) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
                             $stmt->bind_param("sssssssssss", $numeroPedido, $fecha_actual, $nombre, $apellidos, $telefono, $email, $direccion, $localidad, $codigoPostal, $total, $totalConEnvio);
                             $stmt->execute();
-    
+
                             for ($i = 0; $i <= count($carrito_mio) - 1; $i++) {
                                 $articulo_nombre = $carrito_mio[$i]['nombre'];
                                 $articulo_cantidad = $carrito_mio[$i]["cantidad"];
                                 $articulo_precio = $carrito_mio[$i]["precio"];
-    
+
                                 $stmt2 = $conexion->prepare("INSERT INTO pedido_producto (pedido_id, producto_nombre, cantidad, precio) VALUES (?, ?, ?, ?)");
                                 $stmt2->bind_param("ssss", $numeroPedido, $articulo_nombre, $articulo_cantidad, $articulo_precio);
                                 $stmt2->execute();
                             }
-    
+
                             $stmt->close();
                             $stmt2->close();
                             $conexion->close();
-    
-    
+
+
                             // Marcar el pedido como realizado
                             $_SESSION['pedido_realizado'] = true;
                             $_SESSION['referencia_pedido'] = $numeroPedido;
-    
+
                             //Enviar el mail al dueño de la tienda con la confirmación del pedido
                             //Capturar la salida para poder capturar el bucle
                             ob_start();
@@ -338,26 +340,30 @@ function crearPedido(){
                             $lista_productos_email = ob_get_clean();
                             $total_email = $_SESSION["totalSinEnvio"];
                             $total_email_envio = $_SESSION["totalConEnvio"];
-    
+
                             $cabeceras = "MIME-Version: 1.0" . "\r\n";
                             $cabeceras .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                            $cabeceras .= "From: tienda@elrincondelchatin.com\r\n";
+                            $cabeceras .= "Reply-To: tienda@elrincondelchatin.com\r\n";
                             $para = 'aitorjaro11@hotmail.com';
                             $asunto = '¡Nuevo pedido ' . $numeroPedido . '!';
                             $cuerpo = "Tienes un nuevo pedido <b>$numeroPedido</b> de <b>$nombre $apellidos</b>.<br><br><b>Nombre:</b>
         $nombre<br><b>Apellidos:</b> $apellidos<br><b>Teléfono:</b> $telefono<br><b>Email:</b> $email<br><b>Dirección:</b>
         $direccion<br><b>Localidad:</b> $localidad<br><b>Código postal:</b> $codigoPostal<br><br> <b>PRODUCTOS:</b><br><br> $lista_productos_email <br><br><b>TOTAL:</b> $total_email € <br><br><b>TOTAL CON ENVÍO:</b> $total_email_envio €";
-    
+
                             mail($para, $asunto, $cuerpo, $cabeceras);
-    
+
                             //Enviar mail al comprador
                             $cabeceras = "MIME-Version: 1.0" . "\r\n";
                             $cabeceras .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                            $cabeceras .= "From: tienda@elrincondelchatin.com\r\n";
+                            $cabeceras .= "Reply-To: tienda@elrincondelchatin.com\r\n";
                             $para = $_SESSION["email"];
                             $asunto = 'Confirmación de pedido ' . $numeroPedido . ' en El Rincón del Chatín';
                             $cuerpo = "Muchas gracias por comprar en El Rincón del Chatín, <b>$nombre </b>. Tu pedido <b>$numeroPedido</b> ha sido confirmado.<br><br><b>Tu nombre:</b>
         $nombre<br><b>Apellidos:</b> $apellidos<br><b>Teléfono:</b> $telefono<br><b>Email:</b> $email<br><b>Dirección:</b>
         $direccion<br><b>Localidad:</b> $localidad<br><b>Código postal:</b> $codigoPostal<br><br> <b>PRODUCTOS ADQUIRIDOS:</b><br><br> $lista_productos_email <br><br><b>TOTAL:</b> $total_email € <br><br><b>TOTAL CON ENVÍO:</b> $total_email_envio €";
-    
+
                             mail($para, $asunto, $cuerpo, $cabeceras);
                         } else { //Pago denegado
                             echo "El pago no ha sido aceptado. Código de respuesta: $dsResponse";
@@ -369,7 +375,7 @@ function crearPedido(){
                     die("No se recibió respuesta");
                 }
             }
-    
+
         }
     }
 }
@@ -380,53 +386,58 @@ function listar_categorias()
     $categorias = cargar_categorias();
     return $categorias;
 }
-function cargar_categoria_id($id){
+function cargar_categoria_id($id)
+{
     $categorias = array();
-    $con = conexion(); 
+    $con = conexion();
     if ($con) {
         $busqueda = mysqli_query($con, "SELECT * from categorias WHERE categoria = '$id'");
         $categorias = mysqli_fetch_assoc($busqueda);
-        $con->close(); 
+        $con->close();
     }
     return $categorias;
 }
-function listar_pedidos(){
+function listar_pedidos()
+{
     $pedidos = array();
-    $con = conexion(); 
+    $con = conexion();
     if ($con) {
         $busqueda = mysqli_query($con, "SELECT * from pedidos ORDER BY fecha DESC");
         while ($pedido = mysqli_fetch_assoc($busqueda)) {
             $pedidos[] = $pedido;
         }
-        $con->close(); 
+        $con->close();
     }
     return $pedidos;
 }
-function listar_pedido_id($id){
+function listar_pedido_id($id)
+{
     $pedido = array();
-    $con = conexion(); 
+    $con = conexion();
     if ($con) {
         $busqueda = mysqli_query($con, "SELECT * from pedidos WHERE referencia = '$id'");
         $pedido = mysqli_fetch_assoc($busqueda);
-        $con->close(); 
+        $con->close();
     }
     return $pedido;
 }
-function listar_productos_idPedido($idPedido){
+function listar_productos_idPedido($idPedido)
+{
     $productos = array();
-    $con = conexion(); 
+    $con = conexion();
     if ($con) {
         $busqueda = mysqli_query($con, "SELECT * from pedido_producto WHERE pedido_id = $idPedido");
         while ($producto = mysqli_fetch_assoc($busqueda)) {
             $productos[] = $producto;
         }
-        $con->close(); 
+        $con->close();
     }
     return $productos;
 }
-function obtener_hash($nombreUsuario){
+function obtener_hash($nombreUsuario)
+{
     $con = conexion();
-    $sql =  "SELECT clave from usuarios WHERE nombre = '$nombreUsuario'";
+    $sql = "SELECT clave from usuarios WHERE nombre = '$nombreUsuario'";
     $resultado = $con->query($sql);
     if ($resultado) {
         $fila = $resultado->fetch_assoc();
